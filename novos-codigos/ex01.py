896b6797cb872222e9641bbaa51c1ff024e51d02
Antonio Tiburtino <156551571+antoniotiburtino@users.noreply.github.com>
2024-10-18 16:30:52 +0000

    Adicionei variavel nome e uma lista de strings

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,13 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+nome = "Antonio Tiburtino"
+
+frutas =  [ 'banana' ,'maçã', 'abacaxi', 'morango' ]
+
+print(nome)
+
+print(frutas)
+
+
+
+
